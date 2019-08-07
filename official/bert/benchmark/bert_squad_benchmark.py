51ff11b0a11457c02a0195dfdce5dc2540517df2
Zongwei Zhou <zongweiz@google.com>
2019-08-07 16:18:37 -0700

    squad input to int64; selectively enable vlog

--- official/bert/benchmark/bert_squad_benchmark.py
+++ official/bert/benchmark/bert_squad_benchmark.py
@@ -22,9 +22,9 @@ import json
 import os
 import time
 
-os.environ["TF_CPP_VMODULE"] = "meta_optimizer=2"
-os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "1"
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
+os.environ["TF_CPP_VMODULE"] = "meta_optimizer=2,graph_partition=1,stream=1"
+#os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "1"
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
 
 # pylint: disable=g-bad-import-order
 from absl import flags
