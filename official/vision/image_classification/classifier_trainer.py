d6e1ba68e5c2ba7732269ab23561e1f27ab8e8ce
Allen Wang <allencwang@google.com>
2020-05-26 12:41:47 -0700

    revert to HEAD for a run

--- official/vision/image_classification/classifier_trainer.py
+++ official/vision/image_classification/classifier_trainer.py
@@ -301,7 +301,6 @@ def train_and_eval(
       distribution_strategy=params.runtime.distribution_strategy,
       all_reduce_alg=params.runtime.all_reduce_alg,
       num_gpus=params.runtime.num_gpus,
-      num_packs=params.runtime.num_packs,
       tpu_address=params.runtime.tpu)
 
   strategy_scope = distribution_utils.get_strategy_scope(strategy)
