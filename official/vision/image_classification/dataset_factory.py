a8d31fe6386e442ed62fdbb52b96c243b3346cce
Allen Wang <allencwang@google.com>
2020-05-27 13:54:07 -0700

    remove experimental distribute datasets from function

--- official/vision/image_classification/dataset_factory.py
+++ official/vision/image_classification/dataset_factory.py
@@ -284,8 +284,9 @@ class DatasetBuilder:
                      '%d devices.',
                      strategy.num_replicas_in_sync,
                      self.config.num_devices)
-      dataset = strategy.experimental_distribute_datasets_from_function(
-          self._build)
+      dataset = dataset._build()
+      #dataset = strategy.experimental_distribute_datasets_from_function(
+      #    self._build)
     else:
       dataset = self._build()
 
